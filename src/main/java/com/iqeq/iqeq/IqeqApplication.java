23c8fc494e46b195bd0352f09a4e3da6640b81dd
anshitawissen <anshita.makode@wissen.com>
2025-04-07 15:26:36 +0530

    Initial commit

---
+++ src/main/java/com/iqeq/iqeq/IqeqApplication.java
@@ -0,0 +1,13 @@
+package com.iqeq.iqeq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IqeqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IqeqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iqeq/iqeq/IqeqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iqeq.iqeq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IqeqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
